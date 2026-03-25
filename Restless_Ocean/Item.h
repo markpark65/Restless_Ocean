74c8490fab70e5d6f60201a25008547bbf818a76
sumin <sumannii036@gmail.com>
2026-03-25 21:07:33 +0900

    Item 클래스 추가

---
+++ Restless_Ocean/Item.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+class Player;
+
+class Item {
+public:
+    virtual std::string getName() = 0;
+    virtual void use(Player* character) = 0;
+
+    virtual ~Item() {}
+};
+
+class HealthPotion : public Item {
+public:
+    HealthPotion();
+    std::string getName()override;
+    void use(Player* character)override;
+
+private:
+    std::string name;
+    int healthRestore;
+
+};
+
+class AttackBoost : public Item {
+public:
+    AttackBoost();
+    std::string getName()override;
+    void use(Player* character)override;
+private:
+    std::string name;
+    int attackincrease;
+
+};
