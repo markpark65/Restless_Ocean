6e9e35ff8a9d91d11fd369233949a5c91733ac5d
박지석 <qkrwltjr0331@gmail.com>
2026-03-25 20:40:52 +0900

    GameLogger클래스 추가

---
+++ Restless_Ocean/GameLogger.cpp
@@ -0,0 +1,11 @@
+﻿#include "GameLogger.h"
+
+void GameLogger::log(int turn, std::string actor, std::string target, EventType type, int val)
+{
+	//to-do logs에 이벤트 기록.
+}
+
+void GameLogger::printRecentLog(int count)
+{
+	//to-do 최근 count개의 로그를 출력, 이벤트에 맞게 적절한 내용으로.
+}
