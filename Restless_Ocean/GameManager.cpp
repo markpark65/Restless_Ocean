5837f7b11a0dd67fadb40c1b305478bfb9537190
any0509 <annadevelop01@gmail.com>
2026-03-25 20:51:18 +0900

    class GameManager 추가

---
+++ Restless_Ocean/GameManager.cpp
@@ -0,0 +1,11 @@
+#include "GameManager.h"
+
+//구현 전
+
+//Monster* GameManager::generateMonster(int level) {}
+//void GameManager::battle(Player* player) {
+//
+//}
+//void GameManager::displayInventory(Player* player) {
+//
+//}
