5f2146734193712d87f01ba2c457f59b02f7f337
박지석 <qkrwltjr0331@gmail.com>
2026-03-26 21:29:15 +0900

    이벤트 타입 정리 및 1차 기능 구현 완료
    메인에 테스트 함수 추가.

--- Restless_Ocean/GameLogger.cpp
+++ Restless_Ocean/GameLogger.cpp
@@ -1,11 +1,86 @@
 ﻿#include "GameLogger.h"
 
-void GameLogger::log(int turn, std::string actor, std::string target, EventType type, int val)
+
+std::string GameLogger::getMessage(EventLog& log)
 {
-	//to-do logs에 이벤트 기록.
+	std::string message{ "" };
+	switch (log.eventType) {
+	case EventType::Start:
+		message = log.actor + "의 탐험이 시작.";
+		break;
+	case EventType::Battle:
+		message = log.actor + "가 " + log.target +"에게 " + std::to_string(log.value) + "의 데미지를 입힘.";
+		break;
+	case EventType::Death:
+		message = log.actor + "는 " + log.target + "과의 전투에서 사망";
+		break;
+	case EventType::Kill:
+		message = log.actor + "는 " + log.target + "을 처치하였음.";
+		break;
+	case EventType::Move:
+		message = log.actor + "가 구역 " + log.target + "으로 이동.";
+		break;
+	case EventType::LevelUp:
+		message = log.actor + " 레벨 " + std::to_string(log.value) + "으로 상승.";
+		break;
+	case EventType::ObtainItem:
+		message = log.actor + "가 " + log.target + "을(를) 획득.";
+		break;
+	case EventType::UseItem:
+		message = log.actor + "가 " + log.target + "을(를) 사용.";
+		break;
+	case EventType::Return:
+		message = log.actor + "는 육지로 무사히 복귀하였음.";
+		break;
+	}
+	return message;
 }
 
-void GameLogger::printRecentLog(int count)
+
+void GameLogger::log(int turn, EventType type, const std::string& actor)
+{
+	log(turn, type, actor, "", 0);
+}
+
+void GameLogger::log(int turn, EventType type, const std::string& actor, int val)
+{
+	log(turn, type, actor, "", val);
+}
+
+void GameLogger::log(int turn, EventType type, const std::string& actor, const std::string& target)
+{
+	log(turn, type, actor, target, 0);
+}
+
+
+void GameLogger::log(int turn, EventType type, const std::string& actor, const std::string& target, int val)
+{
+	logs.push_back(EventLog{
+		turn,
+		type,
+		actor,
+		target,
+		val
+	});
+}
+
+void GameLogger::printRecentLog(size_t count)
 {
 	//to-do 최근 count개의 로그를 출력, 이벤트에 맞게 적절한 내용으로.
+	int currentidx = 0;
+	//for (auto& log : std::vector<EventLog>(logs.rbegin(), logs.rend())) {
+	for (auto& log : logs) {
+		std::string message = getMessage(log);
+		std::cout << message << '\n';
+
+		if (count <= currentidx)
+			break;
+		else
+			currentidx++;
+	}
+}
+
+void GameLogger::printAllLogs()
+{
+	printRecentLog(logs.size());
 }
--- Restless_Ocean/Restless_Ocean.cpp
+++ Restless_Ocean/Restless_Ocean.cpp
@@ -6,6 +6,28 @@
 #include "MonsterFactory.h"
 #include "Player.h"
 #include "BattleSystem.h"
+#include "GameLogger.h"
+void LoggingTest() {
+    std::cout << "===============================" << '\n';
+    std::cout << "로깅 및 출력 테스트." << '\n';
+    GameLogger& logger = GameLogger::getInstance();
+    // logger.log(현재 턴 수, EventType::이벤트 타입, 행동자, 대상자, 값(회복, 데미지 등));
+    logger.log(1, EventType::Start, "테스터");
+    logger.log(3, EventType::Move, "테스터", "유적1");
+    logger.log(2, EventType::Battle, "테스터", "몬스터더미", 10);
+    logger.log(2, EventType::Battle, "몬스터더미", "테스터", 10);
+    logger.log(2, EventType::Kill, "테스터", "몬스터더미");
+    logger.log(2, EventType::LevelUp, "테스터", 3);
+    logger.log(3, EventType::ObtainItem, "테스터", "포션");
+    logger.log(3, EventType::UseItem, "테스터", "포션");
+    logger.log(3, EventType::Move, "테스터", "유적2");
+    logger.log(4, EventType::Battle, "테스터", "보스더미", 10);
+    logger.log(4, EventType::Battle, "보스더미", "테스터", 999);
+    logger.log(4, EventType::Death, "테스터", "몬스터더미");
+    logger.log(5, EventType::Return, "테스터");
+    logger.printAllLogs();
+    std::cout << "===============================" << '\n';
+}
 
 int main()
 {
