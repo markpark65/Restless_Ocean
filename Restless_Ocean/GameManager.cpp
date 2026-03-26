c4a817d660ddb79c5d5192cc0d4033e71d7842e0
any0509 <annadevelop01@gmail.com>
2026-03-26 15:03:33 +0900

    battle() 함수 작성

--- Restless_Ocean/GameManager.cpp
+++ Restless_Ocean/GameManager.cpp
@@ -1,11 +1,19 @@
 #include "GameManager.h"
+#include "MonsterFactory.h"
+#include "Player.h"
 
-//구현 전
-
-//Monster* GameManager::generateMonster(int level) {}
-//void GameManager::battle(Player* player) {
-//
+//Monster* GameManager::generateMonster(int level) {
+//	 
 //}
+void GameManager::battle(Player* player) {
+	MonsterFactory monsterFactory;
+	Monster* monster = monsterFactory.GenerateMonster();
+
+	while (player->getHp() > 0 && monster->getHealth() > 0) {
+		
+
+	}
+}
 //void GameManager::displayInventory(Player* player) {
 //
 //}
