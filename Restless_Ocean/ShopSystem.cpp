8f24d051d6259e984cdd59ef08ccd1fabcfdb1b0
sumin <sumannii036@gmail.com>
2026-03-27 21:13:50 +0900

    gatcha system

--- Restless_Ocean/ShopSystem.cpp
+++ Restless_Ocean/ShopSystem.cpp
@@ -2,36 +2,74 @@
 #include "Player.h"
 #include "InputSystem.h"
 #include <iostream>
+#include "Random.h"
 using namespace std;
 ShopSystem::ShopSystem() {
-	items_.push_back(std::make_unique<HealthPotion>("체력 포션", 10, 50));
-	items_.push_back(std::make_unique<MaxHpUp>("방수가 잘 되어있는 초코과자", 30, 10));
-	items_.push_back(std::make_unique<OxygenPotion>("산소 포션", 10, 10));
-	items_.push_back(std::make_unique<MaxOxygenUp>("1000년 전의 스노클", 30, 10));
-	items_.push_back(std::make_unique<PressurePotion>("압력 포션", 10, 10));
-	items_.push_back(std::make_unique<MaxPressureUp>("어느 물고기의 부례", 30, 10));
-	items_.push_back(std::make_unique<AttackBoost>("깨진 조개껍데기 목걸이", 30, 10));
+
+	//Level 1
+	items_.push_back(std::make_unique<HealthPotion>("체력 포션", 10, 50,1));
+	items_.push_back(std::make_unique<OxygenPotion>("산소 포션", 10, 10,1));
+	items_.push_back(std::make_unique<PressurePotion>("압력 포션", 10, 10,1));
+
+	//Level 10
+	items_.push_back(std::make_unique<MaxHpUp>("방수가 잘 되어있는 초코과자", 30, 10,10));
+	items_.push_back(std::make_unique<MaxOxygenUp>("1000년 전의 스노클", 30, 10,10));
+	items_.push_back(std::make_unique<MaxPressureUp>("어느 물고기의 부례", 30, 10,10));
+	items_.push_back(std::make_unique<AttackBoost>("깨진 조개껍데기 목걸이", 30, 10,10));
 }
 
 
 void ShopSystem::buyItem(Player& player) {
 	std::cout << "\n물품 목록:\n";
+	int unlockedCount = -1; 
+
+	
 	for (size_t i = 0; i < items_.size(); i++) {
 		std::cout << i << ": ";
 		items_[i]->printInfo();
+
+		if (!items_[i]->isUnlocked(player.getLevel())) {
+			std::cout << " [잠김]";
+		}
+		else {
+			unlockedCount = static_cast<int>(i); 
+		}
+		std::cout << "\n";
 	}
 
+	
+	int gachaIndex = static_cast<int>(items_.size());
+	std::cout << gachaIndex << ": [ 랜덤 아이템 뽑기 가격 : 5G ]";
+	if (player.getLevel() < 10) std::cout << " [잠김]";
+	std::cout << "\n";
+
 	InputSystem inputSys;
-	int index, quantity;
+	int index;
+
+
+	int maxIndex = unlockedCount;
+	if (player.getLevel() >= 10) maxIndex = gachaIndex; 
 
 	std::cout << "구입 물품 번호";
-	index = inputSys.getInputInt(0, static_cast<int>(items_.size() - 1));
+	index = inputSys.getInputInt(0, maxIndex);
+
+	
+	if (index == gachaIndex) {
+		gacha(player);
+		return;
+	}
+	
+	if (!items_[index]->isUnlocked(player.getLevel())) {
+		std::cout << "이 아이템은 아직 구매할 수 없습니다!\n";
+		return;
+	}
 
 	std::cout << "수량 ";
-	quantity = inputSys.getInputInt(1, 99);
+	int quantity = inputSys.getInputInt(1, 99);
 
+	
 	int totalPrice = items_[index]->getPrice() * quantity;
-	if (player.getGold() < items_[index]->getPrice()) {
+	if (player.getGold() < totalPrice) {
 		std::cout << "골드 부족!\n";
 		return;
 	}
@@ -39,7 +77,7 @@ void ShopSystem::buyItem(Player& player) {
 	player.addGold(-totalPrice);
 	for (int i = 0; i < quantity; i++) {
 		player.getInventory().addItem(items_[index]->clone());
-	};
+	}
 
 	std::cout << "구매 완료! 남은 골드: " << player.getGold() << "\n";
 }
@@ -74,3 +112,44 @@ void ShopSystem::sellItem(Player& player) {
 
 	std::cout << "판매 완료! +" << sellPrice << "G\n남은 골드: " << player.getGold() << "\n";
 }
+
+void ShopSystem::gacha(Player& player) {
+	const int cost = 5;
+
+	if (player.getLevel() < 10) {
+		std::cout << "레벨 10 이상부터 이용 가능합니다!\n";
+		return;
+	}
+
+	if (player.getGold() < cost) {
+		std::cout << "골드가 부족합니다!\n";
+		return;
+	}
+
+	player.addGold(-cost);
+
+	std::cout << "\n 뽑기 진행 중...\n";
+	int roll = Random::getRandomValue(0, 99);
+
+	Item* reward = nullptr;
+
+	if (roll < 50) {
+		reward = new HealthPotion("체력 포션", 10, 50, 1);
+	}
+	else if (roll < 80) {
+		reward = new OxygenPotion("산소 포션", 10, 10, 1);
+	}
+	else if (roll < 95) {
+		reward = new PressurePotion("압력 포션", 10, 10, 1);
+	}
+	else {
+		reward = new AttackBoost("깨진 조개껍데기 목걸이", 30, 10, 10);
+	}
+
+	std::cout << "획득 아이템: ";
+	reward->printInfo();
+	std::cout << "\n";
+
+	player.getInventory().addItem(reward);
+}
+
