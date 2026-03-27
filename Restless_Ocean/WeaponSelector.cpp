a6ecc3e260fffdca9b86307427b359482c132565
any0509 <annadevelop01@gmail.com>
2026-03-27 20:41:49 +0900

    WeaponSelector 틀 생성

--- Restless_Ocean/WeaponSelector.cpp
+++ Restless_Ocean/WeaponSelector.cpp
@@ -1,1 +1,14 @@
-﻿#include "WeaponSelector.h"
+﻿#include <iostream>
+#include "WeaponSelector.h"
+WeaponSelector::WeaponSelector()
+{
+	
+}
+WeaponSelector::~WeaponSelector()
+{
+
+}
+void WeaponSelector::selectWeapon() const
+{
+	std::cout << "무기를 선택하세요:\n";
+}
--- Restless_Ocean/WeaponSelector.h
+++ Restless_Ocean/WeaponSelector.h
@@ -1,5 +1,13 @@
 ﻿#pragma once
+#include "Weapon.h"
+
 class WeaponSelector
 {
+public:
+	WeaponSelector();
+	~WeaponSelector();
+	void selectWeapon() const;
+private:
+	//Weapon weapon;
 };
 
