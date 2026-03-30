1f7f5c27515295b0e5442a37a49196b2112f8704
any0509 <annadevelop01@gmail.com>
2026-03-30 16:36:19 +0900

    아이템 인덱스 오류 수정

--- Restless_Ocean/Restless_Ocean.cpp
+++ Restless_Ocean/Restless_Ocean.cpp
@@ -41,6 +41,13 @@ void PrintStatisicsTest() {
 	std::cout << "===============================" << '\n';
 }
 
+void ItemTest() {
+	Player* player = new Player("테스터");
+	player->getInventory().addItem(new HealthPotion("체력 포션", 1, 50, 1));
+	int itemindex = player->getInventory().selectItem();
+	player->getInventory().useItem(itemindex, player);
+}
+
 int main()
 {
 	//콘솔 인코딩 UFT-8 with BOM으로 변경하여 깨짐 방지.
@@ -67,6 +74,8 @@ int main()
     //}
 
 	GameManager::getInstance().run();
+
+	//ItemTest();
 
 	return 0;
 	
